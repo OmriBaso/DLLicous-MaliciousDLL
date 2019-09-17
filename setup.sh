d5a44700cb42177ca32b6ce32ed1e5acf5ced733
J3wker <50461376+J3wker@users.noreply.github.com>
2019-09-17 15:18:14 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,1 @@
-sudo pip3 install termcolor; mkdir /etc/DLLicous; mkdir /etc/DLLicous/output; echo "[+] Done"
+sudo pip3 install termcolor; mkdir /etc/DLLicous; mkdir /etc/DLLicous/output; echo "[+] Done!"
